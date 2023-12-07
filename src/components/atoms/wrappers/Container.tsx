8607cb829458c53a109a70ceab1f7114fe4db23a
白水佑哉 <ys0520@gmail.com>
2023-12-07 12:55:50 +0900

    max width

--- src/components/atoms/wrappers/Container.tsx
+++ src/components/atoms/wrappers/Container.tsx
@@ -1,3 +1,5 @@
 export default function Container(props: { children: React.ReactNode }) {
-  return <div className="container mx-auto px-4">{props.children}</div>;
+  return (
+    <div className="container mx-auto px-4 max-w-md">{props.children}</div>
+  );
 }
